a5b5949b469c3b91d4dee7c73fe943dc6d3e3e46
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2022-12-11 17:31:57 +0100

    coming back after 7 days of flue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent
+    CardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
