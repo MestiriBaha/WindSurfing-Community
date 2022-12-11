9d02d18118b739328a2630c1a09d4aa3bd9bdb29
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2022-12-11 17:44:42 +0100

    removing the card component for now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { CardComponent } from './card/card.component';
 import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardComponent,
+    
     FooterComponent
   ],
   imports: [
